477d2835b741419ed9ccf70c7ade8213932e11c7
Andrew Mishcuk <mischuk2020@gmail.com>
2021-04-03 22:24:17 +0300

    01

---
+++ unity3d/Assets/Scripts/Item/ItemInteractable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInteractable : Interactable
+{
+	public override float StoppingDistance
+	{
+		get { return _interactionDistance * 0.1f; }
+	}
+
+	protected override void Interact()
+	{
+		base.Interact();
+		Debug.Log("I`m sword :) ");
+	}
+}
---
+++ unity3d/Assets/Scripts/NPC/NPCInteractable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+public class NPCInteractable : Interactable
+{
+	protected override void Interact()
+	{
+		base.Interact();
+		Debug.Log("Sup dude");
+	}
+}
